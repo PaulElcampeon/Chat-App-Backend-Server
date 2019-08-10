41c396bbdd2f149cd91938f79d4e58fae820abd6
PaulElcampeon <pauloladele@live.co.uk>
2019-08-10 10:18:55 +0100

    changed data structure for users and id name

--- src/main/java/com/P/G/chatappbackend/models/Room.java
+++ src/main/java/com/P/G/chatappbackend/models/Room.java
@@ -1,31 +1,26 @@
 package com.P.G.chatappbackend.models;
 
-import com.P.G.chatappbackend.cache.NameCache;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Data
 @Document(collection = "ROOMS")
 public class Room {
 
     @Id
-    private String _id;
-    private Map<String, String> users = new HashMap<>();
+    private String roomId;
+    private List<PrivateChatterInfo> users = new ArrayList<>();
     private List<Message> messages = new ArrayList<>();
-    private NameCache nameCache = new NameCache();
 
     public Room() {
 
     }
 
-
-    public Room(String _id) {
-        this._id = _id;
+    public Room(String roomId) {
+        this.roomId = roomId;
     }
 }
