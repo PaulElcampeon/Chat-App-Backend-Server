12e5e66ba5b7099fa27db6ef897912b87d3513b1
PaulElcampeon <pauloladele@live.co.uk>
2019-08-10 10:22:07 +0100

    added extra functionality to return list of names

--- src/main/java/com/P/G/chatappbackend/util/NameCreator.java
+++ src/main/java/com/P/G/chatappbackend/util/NameCreator.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Component
@@ -15,13 +17,26 @@ public class NameCreator {
     private final String[] verbs = {"Towing", "Running", "Swimming", "Farting", "Vomiting"};
     private final StringBuilder stringBuilder = new StringBuilder();
 
-    public ConcurrentHashMap<String, String> createNames() {
+    public ConcurrentHashMap<String, String> createNamesConcurrentHashMap() {
         ConcurrentHashMap<String, String> tempStorage = new ConcurrentHashMap<>(adjectives.length * nouns.length * verbs.length);
         for (String adjective : adjectives) {
             for (String noun : nouns) {
                 for (String verb : verbs) {
                     stringBuilder.delete(0, stringBuilder.length());
                     tempStorage.put(stringBuilder.append(adjective).append(noun).append(verb).toString(), "");
+                }
+            }
+        }
+        return tempStorage;
+    }
+
+    public List<String> createNamesList() {
+        List<String> tempStorage = new ArrayList<>(adjectives.length * nouns.length * verbs.length);
+        for (String adjective : adjectives) {
+            for (String noun : nouns) {
+                for (String verb : verbs) {
+                    stringBuilder.delete(0, stringBuilder.length());
+                    tempStorage.add(stringBuilder.append(adjective).append(noun).append(verb).toString());
                 }
             }
         }
