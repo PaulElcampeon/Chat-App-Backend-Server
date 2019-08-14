9e87cb36131968dc014a1fbda9b2a040d06f24ba
PaulElcampeon <pauloladele@live.co.uk>
2019-08-14 23:28:11 +0100

    removed unneeded methods

--- src/main/java/com/P/G/chatappbackend/repositiories/MessageRepository.java
+++ src/main/java/com/P/G/chatappbackend/repositiories/MessageRepository.java
@@ -1,17 +1,13 @@
 package com.P.G.chatappbackend.repositiories;
 
-import java.util.List;
-
 import com.P.G.chatappbackend.models.Message;
-
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MessageRepository extends MongoRepository<Message, ObjectId> {
-
-    List<Message> findFirst10By_idLessThan(ObjectId _id);
-
-    List<Message> findFirst10ByOrderByTimeSentDesc();
+    
 }
