186f1e0a192525434e2b2f69831f92fa650dd3c9
PaulElcampeon <pauloladele@live.co.uk>
2019-08-19 17:22:47 +0100

    changed configuration

--- src/main/java/com/P/G/chatappbackend/config/WebSocketConfig.java
+++ src/main/java/com/P/G/chatappbackend/config/WebSocketConfig.java
@@ -5,19 +5,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHeaders;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.messaging.simp.stomp.StompCommand;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.GenericMessage;
 import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.messaging.support.NativeMessageHeaderAccessor;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
+import java.util.stream.Collectors;
 
+import static org.springframework.messaging.simp.SimpMessageType.CONNECT;
 import static org.springframework.messaging.simp.SimpMessageType.CONNECT_ACK;
 
 @EnableWebSocketMessageBroker
@@ -52,39 +60,39 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         final StompCommand command = (StompCommand) message.getHeaders().get("stompCommand");
                         final String sessionId = (String) message.getHeaders().get("simpSessionId");
                         final StompHeaderAccessor stompHeaderAccessor = StompHeaderAccessor.wrap(message);
-
-                        if (command == StompCommand.DISCONNECT) {
+                         if (command == StompCommand.DISCONNECT) {
 
                             publicChatRoomService.removeClientFromOnlineUsers(sessionId);
 
                             publicChatRoomService.updateChatRoomWithCurrentUsers();
-
-                        } else if (command == StompCommand.SUBSCRIBE && stompHeaderAccessor.getDestination().equals("/topic/public-room")) {
-
-                            String username;
-
-                            if (stompHeaderAccessor.containsNativeHeader("username")) {
-
-                                username = stompHeaderAccessor.getNativeHeader("username").get(0);
-
-                                if (username.equals("")) {
-
-                                    publicChatRoomService.giveClientName(sessionId);
-
-                                } else {
-
-                                    publicChatRoomService.addClientToOnlineUsers(username, sessionId);
-                                }
-
-                            } else {
-
-                                publicChatRoomService.giveClientName(sessionId);
-
-                            }
-
-                            publicChatRoomService.updateChatRoomWithCurrentUsers();
-
                         }
+
+//                        } else if (command == StompCommand.SUBSCRIBE && stompHeaderAccessor.getDestination().equals("/topic/public-room")) {
+//
+//                            String username;
+//
+//                            if (stompHeaderAccessor.containsNativeHeader("username")) {
+//
+//                                username = stompHeaderAccessor.getNativeHeader("username").get(0);
+//
+//                                if (username.equals("")) {
+//
+//                                    publicChatRoomService.giveClientName(sessionId);
+//
+//                                } else {
+//
+//                                    publicChatRoomService.addClientToOnlineUsers(username, sessionId);
+//                                }
+//
+//                            } else {
+//
+//                                publicChatRoomService.giveClientName(sessionId);
+//
+//                            }
+//
+//                            publicChatRoomService.updateChatRoomWithCurrentUsers();
+//
+//                        }
                         return message;
                     }
                 });
@@ -99,10 +107,27 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
                         final String sessionId = (String) message.getHeaders().get("simpSessionId");
                         final StompHeaderAccessor headerAccessor = StompHeaderAccessor.wrap(message);
                         final SimpMessageType messageType = headerAccessor.getMessageType();
+                        final GenericMessage connectHeader = (GenericMessage)headerAccessor.getHeader(SimpMessageHeaderAccessor.CONNECT_MESSAGE_HEADER);    // FIXME find a way to pass the username to the server
 
                         if (messageType == CONNECT_ACK) {
-
                             final StompHeaderAccessor accessor = StompHeaderAccessor.create(StompCommand.CONNECTED);
+                            final Map<String, List<String>> nativeHeaders = (Map<String, List<String>>) connectHeader.getHeaders().get(SimpMessageHeaderAccessor.NATIVE_HEADERS);
+
+                            if (nativeHeaders.get("username").get(0).equals("")) {
+
+                                String name = publicChatRoomService.giveClientName(sessionId);
+
+                                accessor.addNativeHeader("name", name);
+
+                            } else {
+
+                                String username = nativeHeaders.get("username").get(0);
+
+                                publicChatRoomService.addClientToOnlineUsers(username, sessionId);
+
+                            }
+
+                            publicChatRoomService.updateChatRoomWithCurrentUsers();
 
                             accessor.setSessionId(sessionId);
                             // add custom headers
