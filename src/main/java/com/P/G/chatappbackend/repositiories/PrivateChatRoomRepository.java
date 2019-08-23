066408f430b4a8e69d4770181178f08a51431b95
PaulElcampeon <pauloladele@live.co.uk>
2019-08-23 11:35:28 +0100

    repalced by RoomRepo

--- src/main/java/com/P/G/chatappbackend/repositiories/PrivateChatRoomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.P.G.chatappbackend.repositiories;
-
-import com.P.G.chatappbackend.models.Room;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PrivateChatRoomRepository extends MongoRepository<Room, String> {
-}
