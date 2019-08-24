448903c604f19745e29a57ff77c4f8465da84af7
PaulElcampeon <pauloladele@live.co.uk>
2019-08-24 17:20:39 +0100

    configured Executor

--- src/main/java/com/P/G/chatappbackend/ChatAppBackendApplication.java
+++ src/main/java/com/P/G/chatappbackend/ChatAppBackendApplication.java
@@ -4,10 +4,15 @@ import com.P.G.chatappbackend.services.PublicChatRoomService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import javax.annotation.PostConstruct;
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class ChatAppBackendApplication {
 
     @Autowired
@@ -21,4 +26,16 @@ public class ChatAppBackendApplication {
     public void init() {
         publicChatRoomService.initializeNameCache();
     }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors());
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("Message-Processing-");
+        executor.initialize();
+        return executor;
+    }
+
 }
