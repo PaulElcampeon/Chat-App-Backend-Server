e970248b89a4aad403f33145ad473eed803b40df
PaulElcampeon <pauloladele@live.co.uk>
2019-08-24 19:12:01 +0100

    removed @EnableAsync and taskExecutor bean

--- src/main/java/com/P/G/chatappbackend/ChatAppBackendApplication.java
+++ src/main/java/com/P/G/chatappbackend/ChatAppBackendApplication.java
@@ -1,41 +1,12 @@
 package com.P.G.chatappbackend;
 
-import com.P.G.chatappbackend.services.PublicChatRoomService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-
-import javax.annotation.PostConstruct;
-import java.util.concurrent.Executor;
 
 @SpringBootApplication
-@EnableAsync
 public class ChatAppBackendApplication {
-
-    @Autowired
-    private PublicChatRoomService publicChatRoomService;
 
     public static void main(String[] args) {
         SpringApplication.run(ChatAppBackendApplication.class, args);
     }
-
-    @PostConstruct
-    public void init() {
-        publicChatRoomService.initializeNameCache();
-    }
-
-    @Bean
-    public Executor taskExecutor() {
-        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(Runtime.getRuntime().availableProcessors());
-        executor.setMaxPoolSize(Runtime.getRuntime().availableProcessors());
-        executor.setQueueCapacity(500);
-        executor.setThreadNamePrefix("Message-Processing-");
-        executor.initialize();
-        return executor;
-    }
-
 }
